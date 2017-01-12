fa794b4d741c7c6aafbaa1c6d78eda7cd49ce9eb
Hyoga Yoshida <hyoga0216@gmail.com>
2017-01-12 14:27:57 +0900

    mainつなげて/apiつけた

--- server/server.go
+++ server/server.go
@@ -1,18 +1,51 @@
-package main
+package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
 
-func main(){
-	Run()
-}
+	"github.com/gin-gonic/gin"
+)
 
-
-func Run() {
+func Run() error {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("api/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("api/user/:id", func(c *gin.Context) {
+		var i = c.Param("id")
+		var ids int
+		ids, _ = strconv.Atoi(i)
+		fmt.Println(ids)
+		var name string
+
+		db, err := sql.Open("mysql", "root:password@/supertontonsan")
+		c.JSON(http.StatusInternalServerError, err)
+
+		stmtOut, err := db.Prepare("SELECT user_name FROM users WHERE id=?")
+		c.JSON(http.StatusInternalServerError, err)
+
+		defer stmtOut.Close()
+
+		err = stmtOut.QueryRow(17).Scan(&name)
+		c.JSON(200, gin.H{
+			"ID":       ids,
+			"UserName": name,
+		})
+	})
+
 	r.Run()
+
+	return nil
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
